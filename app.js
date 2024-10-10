2fe884ba910ba8f23a787fbd018fc0345cfac9e8
adeenajaweid96 <adeenajaweid96@gmail.com>
2024-10-10 13:13:05 -0700

    comp

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ document.getElementById("nextSection").addEventListener('click',function(){
     window.location.hash = "section3";
 
 })
+
+// document.getElementById("btnEffect").addEventListener('click',function(){
+    
+
+// })
+
+const button = document.getElementById('btnEffect');
+const popup = document.getElementById('divBlock');
+
+button.addEventListener('click', function () {
+    popup.classList.toggle('hidden');
+});
+
+const backdrop = document.getElementById('backdrop');
+
+button.addEventListener('click', function () {
+    popup.classList.toggle('hidden');
+    backdrop.classList.toggle('hidden');
+});
+
+backdrop.addEventListener('click', function () {
+    popup.classList.add('hidden');
+    backdrop.classList.add('hidden');
+});
